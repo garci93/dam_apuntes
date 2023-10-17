600961a96637a517edde922d9f3bcd52c67063a8
José Joaquín Garci Peña <jojogape@outlook.es>
2023-10-17 14:38:33 +0200

    deint cont regexp

--- DEINT/PruebaExpresionRegular/PruebaExpresionRegular/Program.cs
+++ DEINT/PruebaExpresionRegular/PruebaExpresionRegular/Program.cs
@@ -32,7 +32,7 @@ bool result4 = Regex.IsMatch(frase, patron);
 Match result5 = Regex.Match(frase, @"do\?");
 
 
-//Ejemplo 6 Conjuntos_ [ ] [ - ] [^] [^ - ] \d \D \w \s . \p 
+//Ejemplo 6 Conjuntos_ [ ] [ - ] [^] [^ - ] \d \D \w \s . 
 Match result6 = Regex.Match(frase, @"[Aa]l");
 
 Match result6b = Regex.Match(frase, @"[^Aa]l");
@@ -40,6 +40,7 @@ Match result6b = Regex.Match(frase, @"[^Aa]l");
 Match result6c = Regex.Match(frase, @"\w");
 
 MatchCollection result6d = Regex.Matches(frase, @"\w");
+
 
 //Ejemplo 7 Anchors ^ $ 
 
@@ -49,10 +50,43 @@ Match result7 = Regex.Match(frase, @"^\w");
 
 //Ejemplo 8 Cuantificadores * + ? { } { ,} { , }
 
-string frase8 = "Aquí tienes mi número de telefono (+34)666-66-66-66";
+string frase8 = "Aquí tienes mi número de telefono (+34)666-66-66-66 y mi código 022-991";
 string patron8 = @"\d{3}";
 Regex regex8 = new Regex(patron8);
 MatchCollection match8 = regex8.Matches(frase8);
 int total2 = match8.Count();
 
+//Categorias \p{categoria}
+
+MatchCollection resultado = Regex.Matches(frase8, @"t\p{L}l");
+
+
+//Grupos (  ) 
+
+Match resultado2 = Regex.Match(frase8, @"(\d\d\d)-(\d\d\d)"); 
+
+
 Console.WriteLine();
+
+
+string regexp1 = @"\d{2}/\d{2}/\d{4}";
+string regexp2 = @"^\w+@\w+\.\w{2,3}$";
+string regexp3 = @"^\d{3}\-\d{3}\-\d{4}$";
+string regexp4 = @"^([1-2]?\d{0,2}\.){3}[1-2]?\d{0,2}$";
+string regexp5 = @"^([0-9-afA-F]{2}\-){5}[0-9-afA-F]{2}$";
+char letra = 'a';
+string regexp6 = @"^|\s"+letra+@"\w*";
+
+bool ej1 = Regex.IsMatch("31/12/2001",regexp1);
+bool ej2 = Regex.IsMatch("ejemplo@mail.es",regexp2);
+bool ej3 = Regex.IsMatch("285-636-8535",regexp3);
+bool ej4 = Regex.IsMatch("109.46.5.0",regexp4);
+bool ej5 = Regex.IsMatch("00-A3-73-BC-D4-1F", regexp5);
+bool ej6 = Regex.IsMatch("bardo avanzar regalo sesión ahora",regexp6);
+
+Console.WriteLine(ej1);
+Console.WriteLine(ej2);
+Console.WriteLine(ej3);
+Console.WriteLine(ej4);
+Console.WriteLine(ej5);
+Console.WriteLine(ej6);
