7a9ef34639f944ba9c5c99a183bb5aca21d5ac44
José Joaquín Garci Peña <jojogape@outlook.es>
2023-11-13 12:51:04 +0100

    deint comienzo ficheros

---
+++ DEINT/Ficheros/Ej2/Ej2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficheros
+{
+    internal class Ej2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre del archivo");
+            string ruta = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), Console.ReadLine());
+            Console.WriteLine("Frase a buscar");
+            string frase = Console.ReadLine();
+
+            try
+            {
+                StreamReader streamR = new StreamReader(ruta);
+                int lineaActual = 0;
+                while (!streamR.EndOfStream)
+                {
+                    lineaActual++;
+                    if (streamR.ReadLine().Contains(frase))
+                        Console.WriteLine(lineaActual + ": " + frase);
+                }
+                streamR.Close();
+                streamR.Dispose();
+
+            }
+            catch (Exception ex) { };
+        }
+    }
+}
---
+++ DEINT/Ficheros/Ficheros/Ej1.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Ficheros
+{
+    internal class Ej1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre del archivo 1");
+            string ruta = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), Console.ReadLine());
+            Console.WriteLine("Nombre del archivo 2");
+            string ruta2 = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), Console.ReadLine());
+
+            try
+            {
+                StreamReader streamR = new StreamReader(ruta);
+
+                StreamWriter streamW = new StreamWriter(ruta2);
+                while (!streamR.EndOfStream)
+                {
+                    string s = streamR.ReadLine();
+                    streamW.WriteLine(s);
+                }
+                streamR.Close();
+                streamR.Dispose();
+                streamW.Close();
+                streamW.Dispose();
+
+            }
+            catch (Exception ex) { };
+        }
+    }
+}
