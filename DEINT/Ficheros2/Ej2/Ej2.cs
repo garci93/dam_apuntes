3761d14ab0c287ccdaf7a63682626fc8d4768bd3
José Joaquín Garci Peña <jojogape@outlook.es>
2023-11-14 12:57:19 +0100

    apuntes pmdmo deint ficheros2 empezando

---
+++ DEINT/Ficheros2/Ej2/Ej2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ej2
+{
+    internal class Ej2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el nombre del directorio");
+            DirectoryInfo directorio = new DirectoryInfo(Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), Console.ReadLine()));
+            try
+            {
+                if (directorio.Exists)
+                {
+                    FileInfo[] archivos = directorio.GetFiles();
+
+                    // Comprueba si hay archivos en el directorio
+                    if (archivos != null && archivos.Length > 0)
+                    {
+                        Console.WriteLine("Introduzca la extensión");
+                        string extensionArchivo = Console.ReadLine();
+                        foreach (FileInfo archivo in archivos)
+                        {
+                            if(Regex.IsMatch(archivo.Name,extensionArchivo+"$"))
+                                Console.WriteLine(archivo.Name);
+                        }
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException();
+                    }
+                }
+                else
+                    throw new DirectoryNotFoundException();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("El directorio no existe");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("El directorio está vacío");
+            }
+
+        }
+    }
+}
+
+//error. En caso de que exista, comprobaremos si tiene ficheros dentro, si no fuese
+//así, también se informará al usuario de este hecho. Si tuviese ficheros, debemos
+//mostrar el fichero al que se ha accedido más recientemente.
---
+++ DEINT/Ficheros2/Ficheros2/Ej1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1
+{
+    internal class Ej1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el nombre del directorio");
+            DirectoryInfo directorio = new DirectoryInfo(Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory),Console.ReadLine()));
+            try
+            {
+                if (directorio.Exists)
+                {
+                    FileInfo[] archivos = directorio.GetFiles();
+
+                    // Comprueba si hay archivos en el directorio
+                    if (archivos != null && archivos.Length > 0)
+                    {
+                        DateTime fechaMod = DateTime.MinValue;
+                        string nombreArchivo = "";
+                        foreach (FileInfo archivo in archivos)
+                        {
+                            if (archivo.LastAccessTime > fechaMod)
+                            {
+                                fechaMod = archivo.LastAccessTime;
+                                nombreArchivo = archivo.Name;
+                            }
+                        }
+                        Console.Write(nombreArchivo);
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException();
+                    }
+                }
+                else
+                    throw new DirectoryNotFoundException();
+            } catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("El directorio no existe");
+            } catch (FileNotFoundException)
+            {
+                Console.WriteLine("El directorio está vacío");
+            }
+            
+        }
+    }
+}
