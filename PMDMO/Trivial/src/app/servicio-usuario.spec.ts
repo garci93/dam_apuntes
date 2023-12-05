7c542550bdfdec8d2bcfecad5c382b9de23c9114
José Joaquín Garci Peña <jojogape@outlook.es>
2023-12-05 14:37:01 +0100

    g

--- PMDMO/Trivial/src/app/servicio-usuario.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ServicioUsuario } from './servicio-usuario';
-
-describe('ServicioUsuario', () => {
-  it('should create an instance', () => {
-    expect(new ServicioUsuario()).toBeTruthy();
-  });
-});
