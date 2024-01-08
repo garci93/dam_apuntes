6d24945b63da592c4a011f5064abfa136eb6ce51
José Joaquín Garci Peña <jojogape@outlook.es>
2024-01-08 13:37:01 +0100

    mas maui, empezando python

--- DEINT/ColorPicker/ColorPicker/PaginaInicio.xaml.cs
+++ DEINT/ColorPicker/ColorPicker/PaginaInicio.xaml.cs
@@ -5,10 +5,32 @@ public partial class PaginaInicio : ContentPage
 	public PaginaInicio()
 	{
 		InitializeComponent();
-	}
+        
+    }
 
     private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
+        int valorSliderRed = (int)sliderRed.Value;
+        int valorSliderGreen = (int)sliderGreen.Value;
+        int valorSliderBlue = (int)sliderBlue.Value;
 
+
+        BackgroundColor = Color.FromRgb(valorSliderRed,valorSliderGreen,valorSliderBlue);
+        hexValue.Text = $"Hex Value: #{valorSliderRed:x2}{valorSliderGreen:x2}{valorSliderBlue:x2}";
+    }
+private void randomColor_Clicked(object sender, EventArgs e)
+    {
+        Random random = new Random();
+        int valorRed = random.Next(256);
+        int valorGreen = random.Next(256);
+        int valorBlue = random.Next(256);
+
+
+        BackgroundColor = Color.FromRgb(valorRed, valorGreen, valorBlue);
+        sliderRed.Value = valorRed;
+        sliderGreen.Value = valorGreen;
+        sliderBlue.Value = valorBlue;
+
+        hexValue.Text = $"Hex Value: #{valorRed:x2}{valorGreen:x2}{valorBlue:x2}";
     }
 }
