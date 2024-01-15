2bee94e746bfef89efb13c39fa1d2a2eff8ddba9
José Joaquín Garci Peña <jojogape@outlook.es>
2024-01-15 14:40:38 +0100

    DEINT MVVM

---
+++ DEINT/MVVMDemo/MVVMDemo/MVVM/ViewModels/PersonViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MVVMDemo.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.MVVM.ViewModels
+{
+    public class PersonViewModel
+    {
+        public Person Person { get; set; }
+        public PersonViewModel()
+        {
+            Person = new Person()
+            {
+                Name = "Jose",
+                Age = 1,
+                Married = false,
+                BirthDate = new DateTime(2001, 09, 01),
+                Weight = 100,
+                Lunchtime = new TimeSpan(17, 10, 3)
+            };
+        }
+
+    }
+}
---
+++ DEINT/MVVMDemo/MVVMDemo/MVVM/Views/PersonView.xaml.cs
@@ -0,0 +1,11 @@
+using MVVMDemo.MVVM.ViewModels;
+
+namespace MVVMDemo.MVVM.Views;
+
+public partial class PersonView : ContentPage
+{
+    public PersonView()
+    {
+        InitializeComponent();
+        BindingContext = new PersonViewModel();    }
+}
