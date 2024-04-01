8a9a8a935b4986a3f8051b0159450f6fd5968d70
José Joaquín Garci Peña <jojogape@outlook.es>
2024-04-01 11:14:12 +0200

    coisas

--- DEINT/JoseJoaquinGarciPenaExamen/JoseJoaquinGarciPenaExamen/obj/Debug/net6.0/Ej1.AssemblyInfo.cs
+++ DEINT/JoseJoaquinGarciPenaExamen/JoseJoaquinGarciPenaExamen/obj/Debug/net6.0/Ej1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ej1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14cbb3d353bf68b3d06b39004358aa1eea232ef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ffb42bc0fb6adb943a327b80d3aca960fad8a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ej1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ej1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DEINT/LINQ/LINQ/obj/Debug/net6.0/LINQ.AssemblyInfo.cs
+++ DEINT/LINQ/LINQ/obj/Debug/net6.0/LINQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ffb42bc0fb6adb943a327b80d3aca960fad8a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
