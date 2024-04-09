1150ed96348a5b8e36f50f925e7b44868db85eb6
José Joaquín Garci Peña <jojogape@outlook.es>
2024-04-09 12:43:45 +0200

    cosas deint

--- DEINT/ConsoleApp2/U2Act2Ej3/obj/Debug/net6.0/U2Act2Ej3.AssemblyInfo.cs
+++ DEINT/ConsoleApp2/U2Act2Ej3/obj/Debug/net6.0/U2Act2Ej3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("U2Act2Ej3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8931e49a01446a1e40c27191cd4aab2b8bb8812a")]
 [assembly: System.Reflection.AssemblyProductAttribute("U2Act2Ej3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("U2Act2Ej3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
