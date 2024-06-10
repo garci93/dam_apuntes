3431300e7970d43052fc5d1b7a0418ac7815e227
José Joaquín Garci Peña <jojogape@outlook.es>
2024-06-10 11:14:24 +0200

    repatxo

--- DEINT/Practica10/Practica10/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Practica10.AssemblyInfo.cs
+++ DEINT/Practica10/Practica10/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Practica10.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+92d20b1aa89275c393605e5f13a6102049a34136")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+69423403d1abd24932dffe6d24bebc8e80929c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("MacCatalyst17.2")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("MacCatalyst17.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("MacCatalyst13.1")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
